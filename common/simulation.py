moved all of particle with beacon to common also fixed a stupid radian wrapping problem also implemented sensor timing and delay stuff

common/simulation.py
@@ -41,7 +41,7 @@ class Simulation():
             # transform dynamics to graphics and take only first 2 columns
             pts = (self.dynamics_to_graphics @ pts.T).T[:, :2]
             
-            pygame.draw.lines(self.display_surf, np.array(obj.color) * 0.8, False, pts)
+            pygame.draw.lines(self.display_surf, np.array(obj.color) * 0.8, False, pts, 2)
 
         pygame.display.flip()
 

localize_ultrasonic/estimator.py
@@ -2,6 +2,19 @@ import numpy as np
 from common import matrix_utils
 import motion
 
+shape = np.array([
+    [0.5, 0, 1],
+    [0, 0.5, 1],
+    [-0.5, 0, 1],
+    [0, -0.5, 1],
+    [0.5, 0, 1],
+    [1, 0, 1],
+    [0, 1, 1],
+    [-1, 0, 1],
+    [0, -1, 1],
+    [1, 0, 1]
+])
+
 def jacobian_sense(sa, ma):
     if type(sa) == type(None):
         return np.array([0, 0, 0])
@@ -84,25 +97,11 @@ class EKF():
         return np.diag(L)
 
     def getPoints(self):
-        shape = np.array([
-            [0.5, 0, 1],
-            [0, 0.5, 1],
-            [-0.5, 0, 1],
-            [0, -0.5, 1],
-            [0.5, 0, 1],
-            [1, 0, 1],
-            [0, 1, 1],
-            [-1, 0, 1],
-            [0, -1, 1],
-            [1, 0, 1]
-        ])
-
         cov_truncated = np.copy(self.P)
         cov_truncated[2] = np.array([0, 0, 1])
         trans = matrix_utils.translation2d(self.est[0], self.est[1])
         rot = matrix_utils.rotation2d(self.est[2])
-        shape = trans @ cov_truncated @ rot @ shape.T
-        return shape.T
+        return trans @ cov_truncated @ rot @ shape.T
 
 import scipy
 import scipy.linalg

particle_with_beacon/main.py
@@ -9,8 +9,10 @@ import world_state
 import robot_state
 import sensor
 import state_estimator
+import scipy
+import scipy.linalg
 
-ws = world_state.World(5, 5)
+ws = world_state.World(3, 3)
 rs = robot_state.Robot(0, 0, 0)
 se = state_estimator.EKF([0, 0, 0])
 
@@ -21,6 +23,8 @@ dynamics_to_graphics_ratio = 40 # pixels/unit
 running = True
 cmd = (0, 0)
 clock = pygame.time.Clock()
+cam = sensor.CameraServo(10, rs.getFrame)
+gyr = sensor.Gyroscope(250)
 js = None
 
 v_def = 3
@@ -43,9 +47,7 @@ def on_js_loop():
 def on_key_event(event):
     global running
     global cmd
-    if event.type == pygame.QUIT:
-        running = False
-    elif event.type == pygame.KEYDOWN:
+    if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_KP1:
             cmd = (v_def * -0.75, w_def * 0.75)
         elif event.key == pygame.K_KP2:
@@ -66,88 +68,21 @@ def on_key_event(event):
         cmd = (0, 0)
 
 def on_loop(dt):
-    if cmd == (0, 0):
-        return
     for _ in range(0, dt):
+        cam.step(rs, ws, 1)
+        gyr.step(rs, ws, 1)
         rs.noisyAct(cmd[0], cmd[1], 0.001)
-        se.predict(cmd[0], cmd[1], 0.001, rs.processCov)
-        Z = sensor.getNoisyReading(rs, ws)
-        se.update(ws, Z, sensor.sensorCov)
+    se.predict(cmd[0], cmd[1], dt * 0.001, rs.processCov)
 
-def on_render():
-    display_surf.fill((255, 255, 255))
-
-    z_d, z_tr, z_tg = sensor.getAbsoluteReading(rs, ws)
-
-    bx_graphics, by_graphics = dynamics_to_graphics(ws.bX, ws.bY)
-    rs_xf, rs_yf = dynamics_to_graphics(rs.x + 0.3 * math.cos(rs.t), rs.y + 0.3 * math.sin(rs.t))
-    rx_graphics, ry_graphics = dynamics_to_graphics(rs.x, rs.y)
-    se_xf, se_yf = dynamics_to_graphics(se.mu[0] + 0.3 * math.cos(se.mu[2]), se.mu[1] + 0.3 * math.sin(se.mu[2]))
-    se_gx, se_gy = dynamics_to_graphics(se.mu[0], se.mu[1])
-    sensor_xf, sensor_yf = dynamics_to_graphics(ws.bX - z_d * math.cos(z_tr + rs.t), ws.bY - z_d * math.sin(z_tr + rs.t))
-    gyro_xf, gyro_yf = dynamics_to_graphics(rs.x + 0.3 * math.cos(z_tg), rs.y + 0.3 * math.sin(z_tg))
-
-    # get points to draw the covariance with
-    points = [ np.array([0.1, 0]), np.array([0, 0.1]), np.array([-0.1, 0]), np.array([0, -0.1]) ]
-    cov_truncated = np.array([
-        [se.cov[0][0], se.cov[0][1]],
-        [se.cov[1][0], se.cov[1][1]]
-    ])
-    for i in range(len(points)):
-        points[i] = cov_truncated @ points[i]
-        points[i] += se.mu[:2]
-        points[i] = dynamics_to_graphics(points[i][0], points[i][1])
-
-    # draw beacon:
-    pygame.draw.circle(
-        display_surf,
-        (0, 0, 0),
-        (bx_graphics, by_graphics),
-        int(10)
-    )
-
-    # draw roobit:
-    pygame.draw.line(
-        display_surf,
-        (0, 0, 0),
-        (rx_graphics, ry_graphics),
-        (rs_xf, rs_yf),
-        int(2)
-    )
-
-    # draw sensor reading:
-    # pygame.draw.line(
-    #     display_surf,
-    #     (255, 0, 0),
-    #     (bx_graphics, by_graphics),
-    #     (sensor_xf, sensor_yf),
-    #     int(1)
-    # )
-    # pygame.draw.line(
-    #     display_surf,
-    #     (255, 0, 0),
-    #     (rx_graphics, ry_graphics),
-    #     (gyro_xf, gyro_yf),
-    #     int(1)
-    # )
-    # print(se_gx, se_gy)
-    # draw estimate:
-    pygame.draw.line(
-        display_surf,
-        (255, 0, 255),
-        (se_gx, se_gy),
-        (se_xf, se_yf),
-        int(2)
-    )
-
-    pygame.draw.lines(
-        display_surf,
-        (0, 255, 0),
-        True,
-        points
-    )
+    if cam.hasNewReading and gyr.hasNewReading:
+        cam.hasNewReading = False
+        gyr.hasNewReading = False
+        Z1 = cam.getNoisyReading()
+        Z2 = gyr.getNoisyReading()
+        R = scipy.linalg.block_diag(sensor.CameraCov, sensor.GyroCov)
+        se.update(ws, np.concatenate([Z1, Z2]), R)
 
 sim = simulation.Simulation(width, height)
 sim.initGame()
-sim.addObjects([rs])
+sim.addObjects([rs, ws, se, cam])
 sim.run(on_loop, on_key_event)

particle_with_beacon/robot_state.py
@@ -28,7 +28,7 @@ class Robot():
         dy = v * math.sin(self.t)
         dt = w
         actVector = np.array([dx, dy, dt])
-        return np.random.multivariate_normal(actVector, self.processCov)
+        return np.random.multivariate_normal(actVector, self.processCov * np.linalg.norm(actVector))
 
     def act(self, v, w, dt):
         self.x += v * math.cos(self.t) * dt
@@ -44,6 +44,8 @@ class Robot():
     def getStateVector(self):
         return np.array(self.x, self.y, self.t)
 
+    def getFrame(self):
+        return matrix_utils.translation2d(self.x, self.y) @ matrix_utils.rotation2d(self.t)
+
     def getPoints(self):
-        tf = matrix_utils.translation2d(self.x, self.y) @ matrix_utils.rotation2d(self.t)
-        return matrix_utils.tfPoints(shape, tf)
+        return matrix_utils.tfPoints(shape, self.getFrame())

particle_with_beacon/sensor.py
@@ -2,24 +2,63 @@ import numpy as np
 import math
 from common import matrix_utils
 
-sensorCov = np.array([
-    [0.001, 0, 0],
-    [0, 0.001, 0],
-    [0, 0, 0.001]
+CameraCov = np.array([
+    [1, 0],
+    [0, 1]
 ])
-# sensorCov = np.zeros((2, 2))
+GyroCov = np.array([[np.pi / 180]])
+
+cameraShape = np.array([
+    [0, 0, 1],
+    [1, 0, 1]
+])
 
 class CameraServo():
-    def __init__(self):
-        self.lastReading = [0, 0]
+    # rate is number of readings per second
+    def __init__(self, rate, parentFrame):
+        self.rate = int(1000 / rate)
+        self.time = rate
+        self.lastReading = np.zeros(2)
+        self.hasNewReading = False
+        self.parentFrame = parentFrame
+        self.color = (0, 0, 255)
 
-    def getAbsoluteReading(self, rs, ws):
-        dx = rs.x - ws.bX
-        dy = rs.y - ws.bY
-        dt = math.pi - rs.t + math.atan2(dy, dx)
-        return (math.sqrt(dx * dx + dy * dy), dt, rs.t)
+    def step(self, rs, ws, dt):
+        self.time -= dt
+        if self.time < 0:
+            self.time = self.rate
+            dx = rs.x - ws.bX
+            dy = rs.y - ws.bY
+            dt = math.pi - rs.t + math.atan2(dy, dx)
+            self.lastReading = np.array([math.sqrt(dx * dx + dy * dy), dt])
+            self.hasNewReading = True
 
-    def getNoisyReading(self, rs, ws):
-        d, tr, t = getAbsoluteReading(rs, ws)
-        sensorVector = np.array([d, tr, t])
-        return np.random.multivariate_normal(sensorVector, sensorCov)
+    def getAbsoluteReading(self):
+        return self.lastReading
+
+    def getNoisyReading(self):
+        return np.random.multivariate_normal(self.lastReading, CameraCov * 0.5)
+
+    def getPoints(self):
+        tf = matrix_utils.rotation2d(self.lastReading[1]) @ matrix_utils.scale2d(self.lastReading[0], 0)
+        return matrix_utils.tfPoints(cameraShape, self.parentFrame() @ tf)
+
+class Gyroscope():
+    def __init__(self, rate):
+        self.rate = (1000 / rate)
+        self.time = rate
+        self.lastReading = np.zeros(1)
+        self.hasNewReading = False
+
+    def step(self, rs, ws, dt):
+        self.time -= dt
+        if self.time < 0:
+            self.time = self.rate
+            self.lastReading = np.array([rs.t])
+            self.hasNewReading = True
+
+    def getAbsoluteReading(self):
+        return self.lastReading
+
+    def getNoisyReading(self):
+        return np.random.multivariate_normal(self.lastReading, GyroCov)

particle_with_beacon/state_estimator.py
@@ -1,36 +1,66 @@
 import numpy as np
 import math
+from common import matrix_utils
+
+shape = np.array([
+    [0.5, 0, 1],
+    [0, 0.5, 1],
+    [-0.5, 0, 1],
+    [0, -0.5, 1],
+    [0.5, 0, 1],
+    [1, 0, 1],
+    [0, 1, 1],
+    [-1, 0, 1],
+    [0, -1, 1],
+    [1, 0, 1]
+])
 
 class EKF:
     def __init__(self, initialM):
         self.mu = np.array(initialM, dtype='float64')
         self.cov = np.zeros((3, 3))
+        self.color = (0, 255, 0)
 
     def predict(self, v, w, dt, processCov):
         dx = v * math.cos(self.mu[2]) * dt
         dy = v * math.sin(self.mu[2]) * dt
         dth = w * dt
         self.mu += np.array([dx, dy, dth])
-        self.cov += processCov * dt
+        self.cov += processCov * dt * np.linalg.norm([v, w])
 
     def update(self, ws, Z, measureCov):
         dx = self.mu[0] - ws.bX
         dy = self.mu[1] - ws.bY
         dTotal = dx * dx + dy * dy
 
-        Q = measureCov
+        # dh/dx
         C = np.array([
             [dx / math.sqrt(dTotal), dy / math.sqrt(dTotal), 0],
             [-dy / dTotal, dx/dTotal, -1],
             [0, 0, 1]
         ])
+
+        # h(xhat)
         H = np.array([
             math.sqrt(dTotal),
             math.pi - self.mu[2] + math.atan2(dy, dx),
             self.mu[2]
         ])
 
-        K = self.cov @ C.T @ np.linalg.inv(C @ (self.cov @ C.T) + Q)
-        self.mu += K @ (Z - H)
+        K = self.cov @ C.T @ np.linalg.inv(C @ self.cov @ C.T + measureCov)
+
+        err = Z - H
+
+        # fix stupid radian wrapping stuff
+        if abs(err[1]) > np.pi:
+            err[1] = -(np.sign(err[1]) * np.pi * 2 - err[1])
+
+        self.mu += K @ (err)
         self.cov = (np.eye(3) - (K @ C)) @ self.cov
 
+    def getPoints(self):
+        cov_truncated = np.copy(self.cov)
+        cov_truncated[2] = np.array([0, 0, 1])
+        trans = matrix_utils.translation2d(self.mu[0], self.mu[1])
+        rot = matrix_utils.rotation2d(self.mu[2])
+        return matrix_utils.tfPoints(shape, trans @ cov_truncated @ rot)

particle_with_beacon/world_state.py
@@ -1,8 +1,20 @@
 import numpy as np
+from common import matrix_utils
+
+shape = np.array([
+    [1, 0, 1],
+    [0, -1, 1],
+    [-1, 0, 1],
+    [0, 1, 1],
+    [1, 0, 1]
+]) * np.array([0.2, 0.2, 1])
 
 class World():
     def __init__(self, beaconX, beaconY):
         self.bX = beaconX
         self.bY = beaconY
+        self.color = (255, 0, 0)
 
-    
+    def getPoints(self):
+        tf = matrix_utils.translation2d(self.bX, self.bY)
+        return matrix_utils.tfPoints(shape, tf)